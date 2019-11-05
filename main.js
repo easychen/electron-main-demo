92677b38cacf25bd8ed268968089441dd037cbb3
EasyChen <easychen@qq.com>
2019-11-05 21:42:44 +0800

    init

---
+++ main.js
@@ -0,0 +1,50 @@
+const electron = require('electron');
+const { app , Menu , nativeImage, dialog, shell } = electron;  // Module to control application life.
+const { BrowserWindow }  = electron;  
+const path = require('path');
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+
+let  win;
+function createWindow()
+{
+    win = new BrowserWindow( { width:1200,height:900,resizable:true,frame:false,transparent: true, hasShadow: true, webPreferences: {
+        experimentalFeatures: true,
+        nodeIntegration: true,
+        webSecurity: false
+      } } );
+    win.loadURL( `http://localhost:3000/index.html` );
+    win.on( 'close' , (e)=>
+    {
+        var choice = dialog.showMessageBox(
+            win,
+            {
+                type: 'question',
+                buttons: ['是', '否'],
+                title: '退出确认',
+                message: '确定关闭编辑器？'
+            });
+        
+        if( choice === 0 ) win=null;
+        else e.preventDefault();
+        
+        //if(window.confirm( "确定关闭编辑器？" )) win=null;
+    }  );
+
+    win.webContents.on("new-window", function(event, url) 
+    {
+        event.preventDefault();
+        shell.openExternal(url);
+    });
+    
+}
+
+app.on( 'ready' , ()=>{
+    createWindow();
+});
+
+// app.commandLine.appendSwitch('js-flags', '--max-old-space-size=40960');
+app.on('window-all-closed', () => {
+    app.quit()
+})
